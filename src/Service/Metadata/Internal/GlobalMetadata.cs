bd302d2f46534f7c299649dfcc691b4236f9b87f
Sean Terry <seanterry@outlook.com>
2017-06-29 10:56:36 -0500

    Global metadata definition

---
+++ src/Service/Metadata/Internal/GlobalMetadata.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Fidget.Validation.Addresses.Service.Metadata.Internal
+{
+    class GlobalMetadata : CommonMetadata, IGlobalMetadata
+    {
+        /// <summary>
+        /// Gets the keys of countries for which data is available.
+        /// </summary>
+        
+        [JsonProperty("countries")]
+        [JsonConverter(typeof(TildeDelimitedStringConverter))]
+
+        public IEnumerable<string> Countries { get; set; }
+    }
+}
