4d8e97afe6ca9fc6ece1941448e239268c4fb628
Sean Terry <seanterry@outlook.com>
2017-06-29 10:49:38 -0500

    Base metadata definition

---
+++ src/Service/Metadata/ICommonMetadata.cs
@@ -0,0 +1,15 @@
+﻿namespace Fidget.Validation.Addresses.Service.Metadata
+{
+    /// <summary>
+    /// Defines the common elements of all address metadata.
+    /// </summary>
+
+    public interface ICommonMetadata
+    {
+        /// <summary>
+        /// Gets the key of the region or data record, in the form of a path from parent identifiers to the record key.
+        /// </summary>
+
+        string Id { get; set; }
+    }
+}
---
+++ src/Service/Metadata/Internal/CommonMetadata.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Fidget.Validation.Addresses.Service.Metadata.Internal
+{
+    /// <summary>
+    /// Defines the common elements of all address metadata.
+    /// </summary>
+
+    abstract class CommonMetadata : ICommonMetadata
+    {
+        /// <summary>
+        /// Gets the key of the region or data record, in the form of a path from parent identifiers to the record key.
+        /// </summary>
+        
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
