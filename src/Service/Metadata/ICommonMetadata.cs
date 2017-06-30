22c0b7b0541e1d4e6c329d41dc1d29e5d1b4a7a8
Sean Terry <seanterry@outlook.com>
2017-06-30 11:14:15 -0500

    License and documentation update for common metadata

--- src/Service/Metadata/ICommonMetadata.cs
+++ src/Service/Metadata/ICommonMetadata.cs
@@ -3,12 +3,16 @@
     /// <summary>
     /// Defines the common elements of all address metadata.
     /// </summary>
-
+    
     public interface ICommonMetadata
     {
         /// <summary>
-        /// Gets the key of the region or data record, in the form of a path from parent identifiers to the record key.
+        /// Gets a value that uniquely identifies the data record to the remote service.
         /// </summary>
+        /// <remarks>
+        /// <see cref="https://github.com/googlei18n/libaddressinput/blob/master/common/src/main/java/com/google/i18n/addressinput/common/AddressDataKey.java"/>
+        /// This is represented in the form of a path from parent IDs to the key.
+        /// </remarks>
 
         string Id { get; set; }
     }
