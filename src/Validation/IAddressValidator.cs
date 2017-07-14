10450c86640c4e3042f2f5467ab985f7550be88a
Sean Terry <sean.terry@seaseducation.com>
2017-07-14 17:17:11 -0500

    Cleanup

--- src/Validation/IAddressValidator.cs
+++ src/Validation/IAddressValidator.cs
@@ -1,5 +1,4 @@
-﻿using Fidget.Validation.Addresses.Service.Metadata;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Fidget.Validation.Addresses.Validation
 {
@@ -7,26 +6,6 @@ namespace Fidget.Validation.Addresses.Validation
     /// Defines a means of validating an address against regional metadata.
     /// </summary>
 
-    public interface IAddressValidatorEx
-    {
-        /// <summary>
-        /// Validates the given address against regional metadata.
-        /// </summary>
-        /// <param name="address">Address to validate.</param>
-        /// <param name="global">Global metadata.</param>
-        /// <param name="country">Country metadata.</param>
-        /// <param name="province">Province metadata.</param>
-        /// <param name="locality">Locality metadata.</param>
-        /// <param name="sublocality">Sublocality metadata.</param>
-        /// <returns>The collection of validation errors, if any.</returns>
-
-        IEnumerable<ValidationFailure> Validate( AddressData address, IGlobalMetadata global, ICountryMetadata country, IProvinceMetadata province, ILocalityMetadata locality, ISublocalityMetadata sublocality );
-    }
-
-    /// <summary>
-    /// Defines a means of validating an address against regional metadata.
-    /// </summary>
-    
     public interface IAddressValidator
     {
         /// <summary>
