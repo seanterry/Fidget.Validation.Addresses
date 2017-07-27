9edef2ec6cfe88747b44bf860700452836968237
Sean Terry <sean.terry@seaseducation.com>
2017-07-27 00:12:56 -0500

    Global metadata command

---
+++ src/Metadata/Commands/GlobalMetadataQuery.cs
@@ -0,0 +1,51 @@
+﻿using Fidget.Commander;
+using Fidget.Validation.Addresses.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fidget.Validation.Addresses.Metadata.Commands
+{
+    /// <summary>
+    /// Command for querying global metadata.
+    /// </summary>
+
+    public struct GlobalMetadataQuery : ICommand<GlobalMetadata>
+    {
+        /// <summary>
+        /// Gets the default instance of the command.
+        /// </summary>
+        
+        internal static GlobalMetadataQuery Default { get; } = new GlobalMetadataQuery();
+
+        /// <summary>
+        /// Handler for the command.
+        /// </summary>
+        
+        public class Handler : ICommandHandler<GlobalMetadataQuery, GlobalMetadata>
+        {
+            /// <summary>
+            /// Remote address service client.
+            /// </summary>
+            
+            readonly IServiceClient Client;
+
+            /// <summary>
+            /// Constructs a handler for the command.
+            /// </summary>
+            /// <param name="client">Remote address service client.</param>
+            
+            public Handler( IServiceClient client )
+            {
+                Client = client ?? throw new ArgumentNullException( nameof(client) );
+            }
+
+            /// <summary>
+            /// Executes the command.
+            /// </summary>
+            
+            public async Task<GlobalMetadata> Handle( GlobalMetadataQuery command, CancellationToken cancellationToken ) =>
+                await Client.Query<GlobalMetadata>( "data" );
+        }
+    }
+}
---
+++ test/Metadata/Commands/GlobalMetadataQueryTests.cs
@@ -0,0 +1,54 @@
+﻿using Fidget.Commander;
+using Fidget.Validation.Addresses.Client;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Fidget.Validation.Addresses.Metadata.Commands
+{
+    public class GlobalMetadataQueryTests
+    {
+        Mock<IServiceClient> MockClient = new Mock<IServiceClient>();
+
+        IServiceClient client => MockClient?.Object;
+        ICommandHandler<GlobalMetadataQuery,GlobalMetadata> instance => new GlobalMetadataQuery.Handler( client );
+
+        public class Constructor : GlobalMetadataQueryTests
+        {
+            [Fact]
+            public void Requires_client()
+            {
+                MockClient = null;
+                Assert.Throws<ArgumentNullException>( nameof(client), ()=>instance );
+            }
+        }
+
+        public class Handle : GlobalMetadataQueryTests
+        {
+            GlobalMetadataQuery command;
+            Task<GlobalMetadata> invoke() => instance.Handle( command, CancellationToken.None );
+
+            public static IEnumerable<object[]> ClientResponses => new object[][]
+            {
+                new object[] { null },
+                new object[] { new GlobalMetadata() },
+            };
+
+            [Theory]
+            [MemberData(nameof(ClientResponses))]
+            public async Task Returns_clientResponse( GlobalMetadata response )
+            {
+                MockClient.Setup( _=> _.Query<GlobalMetadata>( "data" ) ).ReturnsAsync( response ).Verifiable();
+
+                command = GlobalMetadataQuery.Default;
+                var actual = await invoke();
+                Assert.Equal( response, actual );
+
+                MockClient.VerifyAll();
+            }
+        }
+    }
+}
