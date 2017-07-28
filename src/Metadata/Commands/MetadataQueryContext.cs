c9d79e25cab7d750cf065161cb7543bdfc34d083
Sean Terry <sean.terry@seaseducation.com>
2017-07-28 09:19:12 -0500

    Country metadata query

---
+++ src/Metadata/Commands/MetadataQueryContext.cs
@@ -0,0 +1,45 @@
+﻿using Fidget.Commander;
+using Fidget.Validation.Addresses.Client;
+using System;
+
+namespace Fidget.Validation.Addresses.Metadata.Commands
+{
+    /// <summary>
+    /// Common metadata query dependencies.
+    /// </summary>
+
+    class MetadataQueryContext : IMetadataQueryContext
+    {
+        /// <summary>
+        /// Gets the remote address service client.
+        /// </summary>
+        
+        public IServiceClient Client { get; }
+
+        /// <summary>
+        /// Gets the key/identifier builder.
+        /// </summary>
+        
+        public IKeyBuilder Builder { get; }
+
+        /// <summary>
+        /// Gets the command dispatcher.
+        /// </summary>
+        
+        public ICommandDispatcher Dispatcher { get; }
+
+        /// <summary>
+        /// Constructs a context for common metadata query dependencies. 
+        /// </summary>
+        /// <param name="client">Remote address service client.</param>
+        /// <param name="builder">Key/identifier builder.</param>
+        /// <param name="dispatcher">Command dispatcher.</param>
+        
+        public MetadataQueryContext( IServiceClient client, IKeyBuilder builder, ICommandDispatcher dispatcher )
+        {
+            Client = client ?? throw new ArgumentNullException( nameof(client) );
+            Builder = builder ?? throw new ArgumentNullException( nameof(builder) );
+            Dispatcher = dispatcher ?? throw new ArgumentNullException( nameof(dispatcher) );
+        }
+    }
+}
---
+++ test/Metadata/Commands/MetadataQueryContextTests.cs
@@ -0,0 +1,63 @@
+﻿using Fidget.Commander;
+using Fidget.Validation.Addresses.Client;
+using Moq;
+using System;
+using Xunit;
+
+namespace Fidget.Validation.Addresses.Metadata.Commands
+{
+    public class MetadataQueryContextTests
+    {
+        Mock<IServiceClient> MockClient = new Mock<IServiceClient>();
+        Mock<IKeyBuilder> MockBuilder = new Mock<IKeyBuilder>();
+        Mock<ICommandDispatcher> MockDispatcher = new Mock<ICommandDispatcher>();
+
+        IServiceClient client => MockClient?.Object;
+        IKeyBuilder builder => MockBuilder?.Object;
+        ICommandDispatcher dispatcher => MockDispatcher?.Object;
+
+        IMetadataQueryContext instance => new MetadataQueryContext( client, builder, dispatcher );
+
+        [Fact]
+        public void Requires_client()
+        {
+            MockClient = null;
+            Assert.Throws<ArgumentNullException>( nameof( client ), () => instance );
+        }
+
+        [Fact]
+        public void Requires_builder()
+        {
+            MockBuilder = null;
+            Assert.Throws<ArgumentNullException>( nameof( builder ), () => instance );
+        }
+
+        [Fact]
+        public void Requires_dispatcher()
+        {
+            MockDispatcher = null;
+            Assert.Throws<ArgumentNullException>( nameof( dispatcher ), () => instance );
+        }
+
+        [Fact]
+        public void Client_returns_expected()
+        {
+            var actual = instance.Client;
+            Assert.Equal( client, actual );
+        }
+
+        [Fact]
+        public void Builder_returns_expected()
+        {
+            var actual = instance.Builder;
+            Assert.Equal( builder, actual );
+        }
+
+        [Fact]
+        public void Dispatcher_returns_expected()
+        {
+            var actual = instance.Dispatcher;
+            Assert.Equal( dispatcher, actual );
+        }
+    }
+}
