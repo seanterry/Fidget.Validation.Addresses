82f2fe1d94837a56e6906b3bbde461b70a6921c4
Sean Terry <sean.terry@seaseducation.com>
2017-08-03 13:21:28 -0500

    Sublocality validator

---
+++ test/Validation/Validators/SublocalityValidatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Fidget.Validation.Addresses.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Fidget.Validation.Addresses.Validation.Validators
+{
+    public class SublocalityValidatorTests : RegionalValidatorTests<SublocalityValidator,LocalityMetadata, SublocalityMetadata>
+    {
+        protected override SublocalityValidator instance => new SublocalityValidator();
+        protected override AddressField field => AddressField.Sublocality;
+
+        protected override void Configure( string value, LocalityMetadata parent, SublocalityMetadata child )
+        {
+            context.Address = new AddressData { Sublocality = value };
+            context.LocalityMetadata = parent;
+            context.SublocalityMetadata = child;
+        }
+    }
+}
